c7bccd72330185c6f6b97e132f9f5f6d0bfa9e49
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-02-06 16:54:12 +0200

    Add validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_name_and_last_name_cannot_be_both_blank
+  validates :gender, inclusion: { in: ["male", "female"] }
+  validate :male_first_name_cannot_be_sue
+
+  private
+    def first_name_and_last_name_cannot_be_both_blank
+      if first_name.blank? && last_name.blank?
+        errors[:base] << "First name and last name can't be both blank"
+      end
+    end
+
+    def male_first_name_cannot_be_sue
+      if gender == "male" && first_name == "Sue"
+        errors[:base] << "Male first name can't be Sue"
+      end
+    end
 end
